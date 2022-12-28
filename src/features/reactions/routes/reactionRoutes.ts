ccbe6cac1e2bbc3d43d529b478fa167e87fe6bac
jabinfs <jabin.floges@gmail.com>
2022-12-28 22:55:50 +0530

    added reaction logic

---
+++ src/features/reactions/routes/reactionRoutes.ts
@@ -0,0 +1,36 @@
+import express, { Router } from 'express';
+import { authMiddleware } from '@global/helpers/auth-middleware';
+import { Get } from '@reactions/controller/get-reactions';
+import { Remove } from '@reactions/controller/remove-reactions';
+import { Add } from '@reactions/controller/add-reaction';
+
+
+class ReactionRoutes {
+  private router: Router;
+
+  constructor() {
+    this.router = express.Router();
+  }
+
+  public routes(): Router {
+    this.router.get('/post/reactions/:postId', authMiddleware.checkAuthentication, Get.prototype.reactions);
+    this.router.get(
+      '/post/single/reaction/username/:username/:postId',
+      authMiddleware.checkAuthentication,
+      Get.prototype.singleReactionByUsername
+    );
+    this.router.get('/post/reactions/username/:username', authMiddleware.checkAuthentication, Get.prototype.reactionsByUsername);
+
+    this.router.post('/post/reaction', authMiddleware.checkAuthentication, Add.prototype.reaction);
+
+    this.router.delete(
+      '/post/reaction/:postId/:previousReaction/:postReactions',
+      authMiddleware.checkAuthentication,
+      Remove.prototype.reaction
+    );
+
+    return this.router;
+  }
+}
+
+export const reactionRoutes: ReactionRoutes = new ReactionRoutes();
