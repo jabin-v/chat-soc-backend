25ec62511a70ac98d687876def4e8c97cf05d950
jabin-v <jabinv03@gmail.com>
2023-01-04 10:54:39 +0530

    add user feature and settings

--- src/shared/globals/helpers/helpers.ts
+++ src/shared/globals/helpers/helpers.ts
@@ -39,5 +39,17 @@ static  parseJson(prop: string): any {
     return dataUrlRegex.test(value);
   }
 
+  static shuffle(list: string[]): string[] {
+    for (let i = list.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [list[i], list[j]] = [list[j], list[i]];
+    }
+    return list;
+  }
+
+  static escapeRegex(text: string): string {
+    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
+  }
+
 
 };
